31c28fd7bce6ce46d92fbbd197c272f2fd89d725
Lucky Khanna <65859909+lkykhanna@users.noreply.github.com>
2022-11-06 11:49:18 +0530

    second commit

---
+++ model/addProducts.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const addProductsSchema = new mongoose.Schema({
+    id: {type: Number, required: true},
+    src: {type: String, required: true},
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: String, required: true},
+    purchaseStatus: {type: Boolean, required: true, default:false}
+}, {
+    collection: 'addProducts'
+})
+
+const model = mongoose.model('addProducts', addProductsSchema)
+
+module.exports = model
